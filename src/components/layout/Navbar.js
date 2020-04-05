54d050b6abb065bcd5f52fa64813e767f8133f89
philcz16 <40038442+philcz16@users.noreply.github.com>
2020-04-05 11:03:55 +0100

    Created a useritem component and a state for the data from the api

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: 'fa fa-github'
+    }
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    }
     render() {
         return (
-            <div>
-                <h1>Navbar</h1>
-            </div>
+            <nav className="navbar bg-primary text-white">
+                <h1><i className={this.props.icon}></i> {this.props.title}</h1>
+            </nav>
         )
     }
 }
